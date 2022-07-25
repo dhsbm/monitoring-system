caf7dc8900809bb59781a1d3eaef3a3ab34da9f1
gosted <9172940+du-hao-111@user.noreply.gitee.com>
2022-07-25 09:14:25 +0800

    init: 初始化项目，配置代码规范

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      components: resolve(__dirname, 'src/components'), // 组件路径别名
+      '@': resolve(__dirname, 'src'), // 根路径别名
+    },
+  },
+})
