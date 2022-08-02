1fbbfd42721fd559cb6c0e4cf2830b88894964bb
Bbbtt04 <2393714045@qq.com>
2022-08-02 10:24:04 +0800

    fix(custom): 添加全局reset

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './mock/mockServe' // 启动Mock服务器
+import '@/assets/style/reset.css'
 
 createApp(App).use(router).use(createPinia()).use(ElementPlus).mount('#app')
